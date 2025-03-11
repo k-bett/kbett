742ac0aa5217eff0acb23417879b1ddc0d401938
k-bett <Kiprotichemmanuel415@gmail.com>
2025-03-11 09:56:25 +0300

    Update routes.dart

--- lib/src/helpers/routes.dart
+++ lib/src/helpers/routes.dart
@@ -4,6 +4,7 @@ import 'package:flutter_news_app/src/pages/homePage/homePage.dart';
 import 'package:flutter_news_app/src/pages/newsApp.dart';
 import 'package:flutter_news_app/src/pages/newsDetail/newsDetailPage.dart';
 import 'package:flutter_news_app/src/pages/profile/profilePage.dart';
+import 'package:kbett/src/widgets/bunny_video_player.dart'; // Import BunnyVideoPlayer
 
 class Routes{
   static Map<String,WidgetBuilder> getRoute(){
@@ -13,6 +14,7 @@ class Routes{
           '/detail': (_) => NewsDetailPage(),
           '/category': (_) => CategoryPage(),
           '/profile': (_) => ProfilePage(),
+          '/video': (_) => BunnyVideoPlayer(videoId: 'YOUR_VIDEO_ID'), // Add video player route
         };
   }
 }
