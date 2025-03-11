98dc378c53790de3633f2804e053ab8992afb92c
k-bett <Kiprotichemmanuel415@gmail.com>
2025-03-11 09:38:30 +0300

    Delete lib/src/lib/src/widgets directory

--- lib/src/lib/src/widgets/bunny_video_player.dart
+++
@@ -1,9 +0,0 @@
-dependencies:
-  flutter:
-    sdk: flutter
-  video_player: ^2.2.10
-  chewie: ^1.2.2
-  dio: ^4.0.0  # For API management
-  path_provider: ^2.0.2  # For accessing local storage
-  shared_preferences: ^2.0.6  # For saving the last viewed position
-  flutter_downloader: ^1.7.1  # For downloading videos
