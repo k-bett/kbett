3eea98a1aea006e42437680b2c421c36a8c82031
k-bett <Kiprotichemmanuel415@gmail.com>
2025-03-11 10:06:38 +0300

    Update routes.dart

--- lib/src/helpers/routes.dart
+++ lib/src/helpers/routes.dart
@@ -2,19 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_news_app/src/pages/category/categoryPage.dart';
 import 'package:flutter_news_app/src/pages/homePage/homePage.dart';
 import 'package:flutter_news_app/src/pages/newsApp.dart';
-import 'package:flutter_news_app/src/pages/newsDetail/newsDetailPage.dart';
 import 'package:flutter_news_app/src/pages/profile/profilePage.dart';
 import 'package:kbett/src/widgets/bunny_video_player.dart'; // Import BunnyVideoPlayer
 
-class Routes{
-  static Map<String,WidgetBuilder> getRoute(){
-    return  <String, WidgetBuilder>{
-          '/': (_) => NewsApp(),
-          '/home': (_) => HomePage(),
-          '/detail': (_) => NewsDetailPage(),
-          '/category': (_) => CategoryPage(),
-          '/profile': (_) => ProfilePage(),
-          '/video': (_) => BunnyVideoPlayer(videoId: 'YOUR_VIDEO_ID'), // Add video player route
-        };
+class Routes {
+  static Map<String, WidgetBuilder> getRoute() {
+    return <String, WidgetBuilder>{
+      '/': (_) => CategoryPage(), // Set CategoryPage as the home page
+      '/home': (_) => CategoryPage(), // Convert CategoryPage to be the home page
+      '/profile': (_) => ProfilePage(),
+      '/video': (_) => BunnyVideoPlayer(videoId: 'YOUR_VIDEO_ID'), // Add video player route
+    };
   }
 }
